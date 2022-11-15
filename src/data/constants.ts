f0a9bc9b888da0f78e3ed14ddcc049d92e27929d
Delavalom <laarvelo@icloud.com>
2022-11-15 13:03:53 -0400

    update the records variable

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,21 +1,29 @@
-import { ReactElement } from 'react';
-import Records from './records.json'
-
+import { ReactElement } from "react";
 export interface Record {
-    id: number;
-    name: string;
-    amount: number;
-    // createAt: Date;
-    // updateAt: Date;
+  id: number;
+  name: string;
+  amount: number;
+  // createAt: Date;
+  // updateAt: Date;
 }
 
-export const RECORDS: Record[] = Records;
-
-export interface UpdateRecord extends Omit<Record, 'id' | 'name'> {}
-
+export interface UpdateRecord extends Omit<Record, "id" | "name"> {}
 export interface Base {
-    getInventory(): Promise<Record[]> | Record[] | ReactElement;
-    createRecord(record: Record): Promise<Record[]> | Record[] | ReactElement;
-    updateRecord(id: Record['id'], changes: UpdateRecord): Promise<Record[] | ReactElement | undefined> | Record[] | undefined | ReactElement;
-    deleteRecord(id: Record['id']): Promise<Record[] | ReactElement | undefined> | Record[] | undefined | ReactElement;
+  getInventory(): Promise<Record[]> | Record[] | ReactElement;
+  createRecord(record: Record): Promise<Record[]> | Record[] | ReactElement;
+  updateRecord(
+    id: Record["id"],
+    changes: UpdateRecord
+  ):
+    | Promise<Record[] | ReactElement | undefined>
+    | Record[]
+    | undefined
+    | ReactElement;
+  deleteRecord(
+    id: Record["id"]
+  ):
+    | Promise<Record[] | ReactElement | undefined>
+    | Record[]
+    | undefined
+    | ReactElement;
 }
--- src/functions/functions.tsx
+++ src/functions/functions.tsx
@@ -1,10 +1,9 @@
-import { RECORDS } from "../data/constants";
+import RECORDS from "../data/records.json";
 import { Record } from "../data/constants";
 import { Base, UpdateRecord } from "../data/constants";
 
 export class Inventory implements Base {
   static instance: Inventory | null = null;
-  private inventory: Record[] = RECORDS;
 
   private constructor() {}
 
@@ -26,7 +25,7 @@ export class Inventory implements Base {
           </tr>
         </thead>
         <tbody>
-          {this.inventory.map(record => (
+          {RECORDS.map(record => (
             <tr key={record.id}>
               <td>{record.id}</td>
               <td>{record.name}</td>
@@ -38,25 +37,25 @@ export class Inventory implements Base {
     )
   }
   createRecord(record: Record) {
-    const getId = this.inventory.some((item) => item.id === record.id);
-    !getId ? this.inventory.push(record) : alert("ID already exist");
+    const getId = RECORDS.some((item) => item.id === record.id);
+    !getId ? RECORDS.push(record) : alert("ID already exist");
     return this.getInventory();
   }
   updateRecord(id: Record["id"], changes: UpdateRecord) {
-    const getId = this.inventory.some((item) => item.id === id);
+    const getId = RECORDS.some((item) => item.id === id);
     if (getId) {
       const index: number = RECORDS.findIndex((item) => item.id === id);
-      this.inventory[index].amount = changes.amount;
+      RECORDS[index].amount = changes.amount;
     } else {
       alert("Not ID was Found");
     }
     return this.getInventory();
   }
   deleteRecord(id: Record["id"]) {
-    const getId = this.inventory.some((item) => item.id === id);
+    const getId = RECORDS.some((item) => item.id === id);
     if (getId) {
       const index: number = RECORDS.findIndex((item) => item.id === id);
-      this.inventory.splice(index, 1);
+      RECORDS.splice(index, 1);
     } else {
       alert("Not ID was Found");
     }
