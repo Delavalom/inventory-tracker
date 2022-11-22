8c4028ce0bff83d0be3d503bff5ac8c6d811a9d7
Delavalom <laarvelo@icloud.com>
2022-11-22 01:00:12 -0400

    Trying adding animations

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonTypes {
 export const Button = ({ title, className, onClick, children }: ButtonTypes) => {
   return (
     <button
-      className={`text-white rounded-lg px-3 py-1 sm::px-5 sm:py-2 text-lg font-semibold bg-indigo-500 transition-colors duration-500 hover:bg-indigo-900 ${className}`}
+      className={`text-white rounded-lg flex flex-row-reverse items-center justify-center px-3 py-1 sm::px-5 sm:py-2 text-lg font-semibold bg-indigo-500 transition-colors duration-500 hover:bg-indigo-900 ${className}`}
       onClick={onClick}
     >
       {title}
